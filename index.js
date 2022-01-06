b26fb3cea8e64c72a76b849fb541fe6fe7daf89e
pv-vighnesh <pvighnesh.vishnu@gmail.com>
2022-01-06 10:26:59 +0530

    Adding React using cdn

---
+++ index.js
@@ -0,0 +1,13 @@
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+ReactDOM.render(
+    <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>,
+    document.getElementById("root")
+)
